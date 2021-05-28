d51ee7f025fccf84f7c6a17120eaab59aaa60b2d
Roger-Heathcote <github@technicalbloke.com>
2021-05-28 10:04:10 +0100

    Added basic email to list of regexes

--- common.js
+++ common.js
@@ -9,15 +9,16 @@ function err(code, userMsg, logMsg, orig = null) {
 }
 
 module.exports = {
+	archivedArgsRegex: /^true$/,
 	err,
-	urlRegex: /[a-zA-Z0-9]{1,12}:\/\/[^-][a-zA-Z0-9.-]{0,253}[^-.](:\d{1,5})?\/[a-zA-Z0-9?/\-._~=]{0,255}/,
 	domainRegex: /^[^-][a-zA-Z0-9.-]{0,253}[^-.]$/,
-	tagRegex: /^[a-z0-9#]+( *[a-z0-9\\/:\-()[\].?#]+)*$/,
+	emailRegex: /(.+)@(.+){2,}\.(.+){2,}/,
+	hasArgsRegex: /^[a-z0-9]+$/,
 	listRegex: /^[a-zA-Z0-9#]+( *[a-zA-Z0-9\\/:\-()[\].?#]+)*$/,
 	listIdRegex: /^(0|A|[a-f0-9]{20})$/,
 	mongoIdRegex: /^[a-f0-9]{24}$/,
 	onlyRegex: /^[a-z0-9#]+( *[a-z0-9\\/:\-()[\].?#]+)*$/,
 	sortOrdersRegex: /^(dateAsc|dateDesc|lenAsc|lenDesc|alphAsc|alphDesc)$/,
-	archivedArgsRegex: /^true$/,
-	hasArgsRegex: /^[a-z0-9]+$/
+	tagRegex: /^[a-z0-9#]+( *[a-z0-9\\/:\-()[\].?#]+)*$/,
+	urlRegex: /[a-zA-Z0-9]{1,12}:\/\/[^-][a-zA-Z0-9.-]{0,253}[^-.](:\d{1,5})?\/[a-zA-Z0-9?/\-._~=]{0,255}/,
 }
